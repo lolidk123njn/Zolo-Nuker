9787ea00e313e2b0dfb11e11c9c8f7b78347bd5d
lolidk123njn <109305348+lolidk123njn@users.noreply.github.com>
2022-07-14 19:32:17 +0200

    Add files via upload

---
+++ requirements.bat
@@ -0,0 +1,21 @@
+@echo off
+
+pip install requests
+pip install asyncio
+pip install discord
+pip install colorama
+pip install pypresence
+pip install aiohttp
+pip install datetime
+pip install psutil
+pip install pyautogui
+pip install selenium
+pip install discord.webhook
+pip install pypresence
+pip install datetime
+pip install pyperclip
+pip install dhooks
+pip install bs4
+pip install plyer
+
+exit
